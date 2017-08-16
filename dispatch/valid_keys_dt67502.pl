4bb1a22e88d0b0c44d1adce0a268ecd3b646acfc
mo <mo@myallocator.com>
2017-08-16 20:14:06 +0200

    perltidy

--- dispatch/valid_keys_dt67502.pl
+++ dispatch/valid_keys_dt67502.pl
@@ -30,36 +30,31 @@ use strict;
 
 use Scalar::Util qw(looks_like_number);
 
-
 # Create list of valid section numbers
 my %valid_keys = ();
-for(my $i = 0; $i < 100; $i++) {
-    my ($a, $b) = split('', sprintf('%02d', $i));
-    next if($a == $b);
-    $valid_keys{$a . ''} = 1;
-    $valid_keys{$b . ''} = 1;
-    $valid_keys{sprintf('%02d', $i) . ''} = 1;
+for ( my $i = 0 ; $i < 100 ; $i++ ) {
+    my ( $a, $b ) = split( '', sprintf( '%02d', $i ) );
+    next if ( $a == $b );
+    $valid_keys{ $a . '' } = 1;
+    $valid_keys{ $b . '' } = 1;
+    $valid_keys{ sprintf( '%02d', $i ) . '' } = 1;
 }
 
 # ASSERT
-die unless(scalar keys %valid_keys == 100);
+die unless ( scalar keys %valid_keys == 100 );
 
 # Create a list of a list with valid section numbers, including sub keys
 #   Example element: ['18', '1', '8']
-my @valid_subkeys = map {
-    length $_ == 1 ? [$_] : [$_, split('', $_)]
-} sort {$a cmp $b} keys %valid_keys;
+my @valid_subkeys = map { length $_ == 1 ? [$_] : [ $_, split( '', $_ ) ] } sort { $a cmp $b } keys %valid_keys;
 
 
 
 # Actually run now
-if($ARGV[0] and looks_like_number($ARGV[0])) {
-    find_valid_keys($ARGV[0]);
+if ( $ARGV[0] and looks_like_number( $ARGV[0] ) ) {
+    find_valid_keys( $ARGV[0] );
+} else {
+    find_valid_keys($_) for ( 1 .. 10 );
 }
-else {
-    find_valid_keys($_) for (1..10);
-}
-
 
 
 
@@ -75,7 +70,6 @@ keys.
 
 =cut
 
-
 sub find_valid_keys {
     my $max = shift;
 
@@ -85,19 +79,19 @@ sub find_valid_keys {
 
     # Recursive testing of all keys
     foreach my $k0 (@valid_subkeys) {
-        my @key = ($k0->[0]);
-        $total_valid_keys += subkey_count($max, @key);
+        my @key = ( $k0->[0] );
+        $total_valid_keys += subkey_count( $max, @key );
     }
 
     print "Valid keys\t: $total_valid_keys\n";
 
     my $total_possible = 1;
-    for(my $i = 0; $i < $max; $i++ ) {
+    for ( my $i = 0 ; $i < $max ; $i++ ) {
         $total_possible *= 100 - $i;
     }
 
     print "Total keys\t: $total_possible\n";
-    print "Result\t\t: " . ($total_valid_keys / $total_possible * 100) . "% valid keys\n\n";
+    print "Result\t\t: " . ( $total_valid_keys / $total_possible * 100 ) . "% valid keys\n\n";
 
 }
 
@@ -117,21 +111,21 @@ sub subkey_count {
     my $key_length = scalar @key;
 
     # Given maximum length reached... we're done
-    if($key_length == $max) {
+    if ( $key_length == $max ) {
         return 1;
     }
 
     my $key_count = 0;
 
-    my @test_key = map { split('', $_) } @key;
+    my @test_key = map { split( '', $_ ) } @key;
 
-    TESTKEY: foreach my $subkeys (@valid_subkeys) {
+TESTKEY: foreach my $subkeys (@valid_subkeys) {
         foreach my $subkey (@$subkeys) {
             my $matches = grep { $_ eq $subkey } @test_key;
-            next TESTKEY if($matches);
+            next TESTKEY if ($matches);
         }
 
-        $key_count += subkey_count($max, @key, $subkeys->[0]);
+        $key_count += subkey_count( $max, @key, $subkeys->[0] );
     }
 
     return $key_count;
